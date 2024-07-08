b27028034a11a525c0e8fead46b724e665d83323
fc8a26 <tcflute64@gmail.com>
2024-07-08 07:47:27 -0400

    Update main.py
    Added tutorial, win/lose detection

--- main.py
+++ main.py
@@ -2,15 +2,30 @@ import sys
 import os
 import platform
 
-board = [0, 0, 0, 0, 0, 0, 0, 0, 0,]
+board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 
 def main():
-    while True:        
-        reload_screen()
-
+    # Tutorial
+    clear_screen()
+    print("Welcome to Tic-Tac-Toe!")
+    print("You play as O's. On your turn, enter a position:\n")
+    print("0 | 1 | 2")
+    print("---------")
+    print("3 | 4 | 5")
+    print("---------")
+    print("6 | 7 | 8\n")
+    input("Click enter to start.")
+    while True:
         # Bot move
-        ...
+        reload_screen()
+        index = 0
+        while True:
+            if board[index] == 0:
+                break
+            else:
+                index += 1
+        board[index] = 1
 
         # Player move
         while True:
@@ -50,17 +65,43 @@ def print_board():
                 print(" | ", end="")
         if i != 2:
             print("\n---------")
-    print("")    
+    print("\n")
+
+
+def get_gamestate():
+    winning_combinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+
+    # Check for a win
+    for combo in winning_combinations:
+        if board[combo[0]] == board[combo[1]] == board[combo[2]] != 0:
+            return board[combo[0]]
+
+    # Check for a draw
+    if not 0 in board:
+        return 0
 
 
 def reload_screen():
     clear_screen()
     print_board()
-    print("")    
-    if not 0 in board:
-        input("Game over")
-        clear_screen()
-        sys.exit()
     
+    # Check for game over
+    gamestate = get_gamestate()
+    if gamestate == 1:
+        sys.exit("Bot wins")
+    elif gamestate == 2:
+        sys.exit("Player wins")
+    elif gamestate == 0:
+        sys.exit("Draw")
+
 
 main()
